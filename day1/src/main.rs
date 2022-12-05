748591e7e51d3d7524f30f02e35c43ef8569d990
danielk <daniel.kastinen@irf.se>
2022-12-05 14:44:02 +0100

    added my comments

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,26 +9,27 @@ i feel like a toddler who is just learning how to use my hands
 */
 fn main() -> Result<(), Error> {
     let path = Path::new("input.txt");
-    let input = File::open(&path)?;
+    let input = File::open(&path)?; /*the ? operator is fantastic*/
 
     let buffered = BufReader::new(input);
     let mut vec = Vec::new();
     let mut current_val: i32 = 0;
 
     for line in buffered.lines() {
-        let line_checked = line?;
+        let line_checked = line?; /* there has to be a better pattern for loops than this? */
+
         if line_checked.len() == 0 {
             vec.push(current_val);
             current_val = 0;
             println!("PUSHING VALUE!");
         }
         else {
-            current_val += line_checked.parse::<i32>().unwrap();
+            current_val += line_checked.parse::<i32>().unwrap(); /* now im getting the whole ::<> thing, its just templates again! */
         }
         println!("{}", current_val);
     }
 
-    let max_value = vec.iter().max().unwrap();
+    let max_value = vec.iter().max().unwrap(); /* it took me SO long to figure out unwrap is ? for Options*/
 
     println!("MAX VALUE: {}", max_value);
 
@@ -39,9 +40,9 @@ fn main() -> Result<(), Error> {
     for n in 0..top_number {
         println!("adding {} to the sum...", vec[vec.len() - n - 1]);
         top_vals += vec[vec.len() - n - 1];
-    }
+    } /* the .. feels a bit sacrilegious */
 
     println!("SUM OF TOP {}: {}", top_number, top_vals);
 
-    Ok(())
+    Ok(()) /* nice */
 }
