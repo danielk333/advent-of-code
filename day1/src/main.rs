cba6037c204997d633de7beaffb2f93ca974a8ff
danielk <daniel.kastinen@irf.se>
2022-12-06 10:02:58 +0100

    refactored with things i learned

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead, Error};
+use std::fs::read_to_string;
+use std::io::{Error};
 use std::path::Path;
 
 /*
@@ -9,38 +9,39 @@ i feel like a toddler who is just learning how to use my hands
 */
 fn main() -> Result<(), Error> {
     let path = Path::new("input.txt");
-    let input = File::open(&path)?; /*the ? operator is fantastic*/
 
-    let buffered = BufReader::new(input);
+    /*the ? operator is fantastic*/
+    let data = read_to_string(path)?;
     let mut vec = Vec::new();
     let mut current_val: i32 = 0;
 
-    for line in buffered.lines() {
-        let line_checked = line?; /* there has to be a better pattern for loops than this? */
-
-        if line_checked.len() == 0 {
+    for line in data.lines() {
+        if line.is_empty() {
             vec.push(current_val);
             current_val = 0;
             println!("PUSHING VALUE!");
         }
         else {
-            current_val += line_checked.parse::<i32>().unwrap(); /* now im getting the whole ::<> thing, its just templates again! */
+            /* now im getting the whole ::<> thing, its just templates again! */
+            current_val += line.parse::<i32>().unwrap();
         }
         println!("{}", current_val);
     }
+    if current_val != 0 {
+        vec.push(current_val);
+        println!("PUSHING VALUE!");
+    }
 
-    let max_value = vec.iter().max().unwrap(); /* it took me SO long to figure out unwrap is ? for Options*/
+    /* it took me SO long to figure out unwrap is ? for Options*/
+    let max_value = vec.iter().max().unwrap(); 
 
     println!("MAX VALUE: {}", max_value);
 
     vec.sort();
 
     let top_number = 3;
-    let mut top_vals: i32 = 0;
-    for n in 0..top_number {
-        println!("adding {} to the sum...", vec[vec.len() - n - 1]);
-        top_vals += vec[vec.len() - n - 1];
-    } /* the .. feels a bit sacrilegious */
+    let n = vec.len();
+    let top_vals = vec[(n - top_number - 1)..(n - 1)].iter().sum::<i32>();
 
     println!("SUM OF TOP {}: {}", top_number, top_vals);
 
