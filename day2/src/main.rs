6577b6f3410235de067d134da58e8d9ba7f84dbb
danielk <daniel.kastinen@irf.se>
2022-12-06 15:21:21 +0100

    day two part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,20 +10,20 @@ fn read_data() -> String {
 
 /*
 match outcome matrix
-          Rock Paper Scissors (opponent)
-Rock       3     0      6
-Paper      6     3      0
-Scissors   0     6      3
-(you)
+          Rock Paper Scissors (you)
+Rock       3     6      0
+Paper      0     3      6
+Scissors   6     0      3
+(opponent)
 
 betting values
          Rock Paper Scissors
 Score      1     2      3
 */
 const MATCH_RESULT: [[u32; 3]; 3] = [
-    [3,0,6],
-    [6,3,0],
-    [0,6,3],
+    [3,6,0],
+    [0,3,6],
+    [6,0,3],
 ];
 const MOVE_SCORE: [u32; 3] = [1,2,3];
 
@@ -32,24 +32,32 @@ fn resolve_game(
     p2_move: usize, 
 ) -> (u32, u32) {
     (
-        MATCH_RESULT[p1_move][p2_move] + MOVE_SCORE[p1_move],
-        MATCH_RESULT[p2_move][p1_move] + MOVE_SCORE[p2_move],
+        MATCH_RESULT[p2_move][p1_move] + MOVE_SCORE[p1_move],
+        MATCH_RESULT[p1_move][p2_move] + MOVE_SCORE[p2_move],
     )
 }
 
+fn find_move(p2_move: u32, p1_score: u32) -> u32 {
+    let scores = MATCH_RESULT[p2_move as usize];
+    let index = scores.iter().position(|&x| x == p1_score).unwrap();
+    return index as u32;
+}
+
 fn parse_data(data: &str) -> (Vec<u32>, Vec<u32>) {
-    let mut move_translate: HashMap<char, u32> = HashMap::new();
-    let mut opponent_move: Vec<u32> = Vec::new();
-    let mut my_move: Vec<u32> = Vec::new();
+    let mut move_id: HashMap<char, u32> = HashMap::new();
+    let mut outcome_score: HashMap<char, u32> = HashMap::new();
+    let mut p1_moves: Vec<u32> = Vec::new();
+    let mut p2_moves: Vec<u32> = Vec::new();
 
-    /* Map to translate into consistent game action IDs */
-    move_translate.insert('X', 0);
-    move_translate.insert('Y', 1);
-    move_translate.insert('Z', 2);
+    /* Decode input data move ID */
+    move_id.insert('A', 0);
+    move_id.insert('B', 1);
+    move_id.insert('C', 2);
 
-    move_translate.insert('A', 0);
-    move_translate.insert('B', 1);
-    move_translate.insert('C', 2);
+    /* Decode input data outcome score */
+    outcome_score.insert('X', 0);
+    outcome_score.insert('Y', 3);
+    outcome_score.insert('Z', 6);
 
     for line in data.lines() {
         if line.is_empty() {
@@ -58,13 +66,18 @@ fn parse_data(data: &str) -> (Vec<u32>, Vec<u32>) {
         let parts: Vec<&str> = line.split(' ').collect();
 
         let c: char = parts[0].chars().next().unwrap();
-        opponent_move.push(*move_translate.get(&c).unwrap());
+        let p2_move: u32 = *move_id.get(&c).unwrap();
+        p2_moves.push(p2_move);
 
         let c: char = parts[1].chars().next().unwrap();
-        my_move.push(*move_translate.get(&c).unwrap());
+        let p1_score: u32 = *outcome_score.get(&c).unwrap();
+        let p1_move: u32 = find_move(p2_move, p1_score);
+        p1_moves.push(p1_move);
+
+        /*println!("{}", line);*/
     }
 
-    return (opponent_move, my_move)
+    return (p2_moves, p1_moves)
 }
 
 fn main() {
