fb01353d8e0c783fbc6f5048e30cb3f971161af8
Daniel Kastinen <waxarn333@gmail.com>
2023-12-17 12:31:11 +0100

    c implementation

--- 2023/day1/c-impl/main.c
+++ 2023/day1/c-impl/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    FILE* cursor;
+    char ch;
+    char first_num = '\0';
+    char last_num;
+    int total = 0;
+
+    cursor = fopen("../input", "r");
+    do {
+        ch = fgetc(cursor);
+        if (ch >= '0' && ch <= '9') {
+            if (first_num == '\0') {
+                first_num = ch;
+            }
+            last_num = ch;
+        }
+        else if (ch == '\n') {
+            total += ((int)(first_num - '0'))*10 + ((int)(last_num - '0'));
+            first_num = '\0';
+        }
+    } while (ch != EOF);
+
+    printf("%d", total);
     return 0;
 }
