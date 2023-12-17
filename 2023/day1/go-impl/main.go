8d38af15ac20c76e44f77ce8eab3227606a756d0
Daniel Kastinen <waxarn333@gmail.com>
2023-12-17 13:03:53 +0100

    go implementation, this is very quick once you know the solution

--- 2023/day1/go-impl/main.go
+++ 2023/day1/go-impl/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "strings"
+)
 
 func main() {
-    fmt.Println("hello world")
+    var first_num rune = '\x00';
+    var last_num rune;
+    var total int = 0;
+
+    data, _ := os.ReadFile("../input")
+    str_data := string(data[:])
+    lines := strings.Split(str_data, "\n")
+    for i := 0; i < len(lines); i++ {
+        line := strings.TrimRight(lines[i], "\n")
+        if len(line) == 0 {
+            continue
+        }
+        for _, char := range line {
+            if char >= '0' && char <= '9' {
+                if first_num == '\x00' {
+                    first_num = char
+                }
+                last_num = char
+            }
+        }
+        total += int(first_num - '0')*10 + int(last_num - '0')
+        first_num = '\x00'
+    }
+    fmt.Println(total)
 }
